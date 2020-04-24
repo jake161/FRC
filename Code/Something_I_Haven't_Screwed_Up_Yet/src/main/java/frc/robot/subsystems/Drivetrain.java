ea469c81a1d4bf1cf52ef1128e271a89e905b701
jake161 <62108812+jake161@users.noreply.github.com>
2020-04-24 01:02:56 -0400

    Add files via upload

---
+++ Code/Something_I_Haven't_Screwed_Up_Yet/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  /**
+   * Creates a new Drivetrain.
+   */
+   Talon talonFL=null;
+   Talon talonFR=null;
+   Talon talonRL=null;
+   Talon talonRR=null;
+
+  public Drivetrain() {
+
+    
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
